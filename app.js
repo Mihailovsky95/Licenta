f0f69bf0ffeac02476419c3baae23899ad12cef1
Mihailovsky95 <mihail.urzica@yahoo.com>
2022-06-09 23:32:55 +0300

    all

--- app.js
+++ app.js
@@ -385,11 +385,13 @@ app.get("/video6", function(req, res){
  videoStream.pipe(res);
  });
 
-let port = process.env.PORT;
-if(port == null || port == ""){
-  port = 3000;
-}
-
-app.listen(port, function() {
-  console.log("Server has started");
+// let port = process.env.PORT;
+// if(port == null || port == ""){
+//   port = 3000;
+// }
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
+// app.listen(port, function() {
+//   console.log("Server has started");
+// });
